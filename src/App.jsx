1cc010bbb37490f18c135e083dc9679515efa056
Debjyoti <majumderdebjyoti8@example.com>
2025-03-18 19:35:13 +0530

    Updated homepage text

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-function App() {
-  return (
-    <div>
-      <h1>Welcome to My React App! 🚀</h1>
-      <p>Let's build something awesome.</p>
-      <p>Let's update. </p>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div>
+//       <h1>Welcome to My React App! 🚀</h1>
+//       <p>Let's build something awesome.</p>
+//       <p>Let's update. </p>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Heading = React.createElement(
+  "h1",
+  {id: "heading"},
+  "Hello World through React"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Heading/>);
